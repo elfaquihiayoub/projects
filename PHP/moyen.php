f5978b9c072e99f47f1601fd7fb5a56721d315c8
elfaquihi_ayoub <elfaquihi.ayoub@gmail.com>
2026-02-03 16:01:45 +0100

    ..

--- PHP/moyen.php
+++ PHP/moyen.php
@@ -8,62 +8,70 @@ if ($_SERVER["REQUEST_METHOD"]==="POST") {
     $moyenne= 0;
 
     if (empty($note_1) || empty($note_2) ||empty($note_3) || empty($nom) ) {
-        echo "saisir tous les champes";
+        echo "saisir tous les champes!!!!!";
     }
     else{
         $notes=[$note_1,$note_2,$note_3];
-    }
-
-   
-    foreach ($notes as $note ) {
+        foreach ($notes as $note ) {
+            if ($note <=0  && $note >=20){
+                echo " les notes ne sont pas entre 0 et 20 ";
+                break;
+            }
+        }
+           foreach ($notes as $note ) {
         if ($note <=0  && $note >=20){
             echo " les notes ne sont pas entre 0 et 20 ";
             break;
         }
+    }function calculerMoyenne($notes) {
+        if(count($notes)>0){
+         global $moyenne;
+         $moyenne=0;
+         $moyenne= array_sum($notes)/count($notes);
+         return $moyenne;
+        }
+        else{
+         echo "error";
+        }
+     
+     }
+     
+     
+     function getMention($moyenne) {
+         if ($moyenne >= 18 && $moyenne <= 20) {
+             echo "Vous êtes admis avec mention Excellent";
+         }
+     
+         elseif ($moyenne >= 16 && $moyenne < 18) {
+             echo "Vous êtes admis avec mention Très Bien";
+         }
+     
+         elseif ($moyenne >= 14 && $moyenne < 16) {
+             echo "Vous êtes admis avec mention Bien";
+         }
+     
+         elseif ($moyenne >= 12 && $moyenne < 14) {
+             echo "Vous êtes admis avec mention assez_bien";
+         }
+         elseif ($moyenne >= 10 && $moyenne < 12) {
+             echo "Vous êtes admis avec mention Passable";
+         }
+         else { 
+             echo "Vous n'êtes pas admis !!!";
+         }
+     }
+     calculerMoyenne($notes);
+     getMention($moyenne);
+
     }
+
+   
+ 
 
 }
 
 
-function calculerMoyenne($notes) {
-   if(count($notes)>0){
-    global $moyenne;
-    $moyenne=0;
-    $moyenne= array_sum($notes)/count($notes);
-    return $moyenne;
-   }
-   else{
-    echo "error";
-   }
 
-}
-
-
-function getMention($moyenne) {
-    if ($moyenne >= 18 && $moyenne <= 20) {
-        echo "Vous êtes admis avec mention Excellent";
-    }
-
-    elseif ($moyenne >= 16 && $moyenne < 18) {
-        echo "Vous êtes admis avec mention Très Bien";
-    }
-
-    elseif ($moyenne >= 14 && $moyenne < 16) {
-        echo "Vous êtes admis avec mention Bien";
-    }
-
-    elseif ($moyenne >= 12 && $moyenne < 14) {
-        echo "Vous êtes admis avec mention assez_bien";
-    }
-    elseif ($moyenne >= 10 && $moyenne < 12) {
-        echo "Vous êtes admis avec mention Passable";
-    }
-    else { 
-        echo "Vous n'êtes pas admis !!!";
-    }
-}
-calculerMoyenne($notes);
-getMention($moyenne);
 
 
 
